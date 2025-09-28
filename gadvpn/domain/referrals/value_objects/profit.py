1ab7f81274b26c7e2c9ddfe7a33940e11b3f92d6
aa08d <alisultanovdzamaludin0@gmail.com>
2025-09-28 22:50:42 +0300

    feat: Referral profit add method

--- gadvpn/domain/referrals/value_objects/profit.py
+++ gadvpn/domain/referrals/value_objects/profit.py
@@ -7,6 +7,9 @@ class Profit(ValueObject):
     def __init__(self, profit: Decimal = Decimal(0)) -> None:
         self._profit = profit
 
+    def add(self, amount: Decimal) -> None:
+        self._profit = self._profit + amount
+
     @property
     def value(self) -> Decimal:
         return self._profit
